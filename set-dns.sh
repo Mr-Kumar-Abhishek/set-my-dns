59def283b58adadb9a1ae55b5dbc5cf47835ac4d
Abhishek Kumar <kumar.abhishek@techie.com>
2015-12-25 08:38:32 +0530

    add bash script

---
+++ set-dns.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+write_to_file(){
+	local file="/etc/resolv.conf"
+	
+	while true; do 
+		DNS_SERVER1="nameserver 208.67.222.222" #new dns
+		DNS_SERVER2="nameserver 208.67.220.220" #new dns 
+		if [ ! -f "$file" ]; then
+			touch "$file"
+		else
+			if  grep "192.168.43.1" $file
+			 then
+				rm "$file"
+				touch "$file"
+
+				echo "$DNS_SERVER1" > "$file";
+				echo "$DNS_SERVER2" >> "$file";
+			fi
+		fi
+		sleep 1
+	done
+}
+
+write_to_file
